8f226d841ce83cdf8e36660710b301b313321c2b
Fiardika <81453470+Fiardika@users.noreply.github.com>
2022-10-10 20:29:34 +0700

    Update server.js
    fix root path + add trust proxy

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('trust proxy', true)
+
 app.get("/", function (req, res) {
     console.log(req.socket.remoteAddress);
     console.log(req.ip);
